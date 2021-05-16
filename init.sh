936c21088d749ef03d944e0df22860626acfd5ce
gsobell <82414189+gsobell@users.noreply.github.com>
2021-05-16 13:21:33 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-# Workaround to make sure script behaves and is excuted in bash.
+#!/bin/bash
 curl https://raw.githubusercontent.com/gsobell/deployrable/home/deployrable.sh > deployrable.sh ;
 chmod +x ./deployrable.sh ; ./deployrable.sh
