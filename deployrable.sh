3720498b2a035dac70ab84fca76ebbd272b7fb34
Moshe Krumbein <moshekrum@gmail.com>
2021-05-19 16:06:29 +0300

    changed indentation for improved readability

--- deployrable.sh
+++ deployrable.sh
@@ -29,49 +29,49 @@ mv -fv dotfiles/.i3/config		$HOME/.i3/config
 # Setting natural scrolling > Use xmodmap instead, look into libinput
 
 if ! command -v paru &> /dev/null
-	then
-		echo 'Refreshing databases'
-		sudo pacman -Syy
-		echo 'Installing Paru'
-		sudo pacman -S --needed base-devel
-		git clone https://aur.archlinux.org/paru.git
-		cd paru
-		makepkg -si		
-		cd $TEMP
-	else    
-		echo 'Paru already installed, refreshing databases'
-		sudo pacman -Syy
+then
+    echo 'Refreshing databases'
+    sudo pacman -Syy
+    echo 'Installing Paru'
+    sudo pacman -S --needed base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si		
+    cd $TEMP
+else    
+    echo 'Paru already installed, refreshing databases'
+    sudo pacman -Syy
 fi
 
 echo "Install packages at this time? (Y/n)"
 read yn
-	
+
 while true; do
-	case $yn in
-	[Yy]* ) ls dotfiles/packlist > packversion.txt
-	select PACKLIST in $(cat packversion.txt) exit; do
-	case $PACKLIST in
-	exit) echo "Exiting."
-	break ;;
-	*) echo "$PACKLIST"
-	echo Installing "$PACKLIST";
-	paru -S - < dotfiles/packlist/"$PACKLIST"
-	rm packversion.txt
-	esac
-	done; break;;
-	[Nn]* ) break;;
-	* ) echo "Please answer yes or no."; echo "Install packages now (Y/n)?"; read yn;;
-	esac
+    case $yn in
+        [Yy]* ) ls dotfiles/packlist > packversion.txt
+            select PACKLIST in $(cat packversion.txt) exit; do
+                case $PACKLIST in
+                    exit) echo "Exiting."
+                        break ;;
+                    *) echo "$PACKLIST"
+                        echo Installing "$PACKLIST";
+                        paru -S - < dotfiles/packlist/"$PACKLIST"
+                        rm packversion.txt
+                esac
+            done; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no."; echo "Install packages now (Y/n)?"; read yn;;
+    esac
 done
 
 if ! command -v nitrogen &> /dev/null
-	then
-		echo 'Not setting wallaper'
+then
+    echo 'Not setting wallaper'
 
-	else
-		curl https://unsplash.com/photos/Dksk8szLRN0/ > .default_wallpaper.png
-		nitrogen $HOME/.default_wallpaper.png
-		echo 'Wallpaper set.'
+else
+    curl https://unsplash.com/photos/Dksk8szLRN0/ > .default_wallpaper.png
+    nitrogen $HOME/.default_wallpaper.png
+    echo 'Wallpaper set.'
 
 fi
 
