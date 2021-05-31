d77f9a50fb19f588b3b164634da745f92de0f682
gsobell <82414189+gsobell@users.noreply.github.com>
2021-05-31 16:38:18 -0400

    Added diff function to PACKLIST

--- deployrable.sh
+++ deployrable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'deployrable v0.1'
+echo 'deployrable v0.2'
 sleep 1
 echo 'MIT License, Copyright (c) 2021 gsobell'
 clear
@@ -19,12 +19,12 @@ git clone https://github.com/gsobell/dotfiles.git
 # Initial dotfile deployment
 mkdir -pv $HOME/.i3
 
-mv -fv dotfiles/.Xresources	    $HOME/.Xresources
-mv -fv dotfiles/.bashrc		    $HOME/.bashrc
-mv -fv dotfiles/.bash_profile	$HOME/.bash_profile
-mv -fv dotfiles/.vimrc		    $HOME/.vimrc
-mv -fv dotfiles/.i3/config		$HOME/.i3/config
-mv     dotfiles/.config/*          $HOME/.config
+mv -fv $TEMP/dotfiles/.Xresources	$HOME/.Xresources
+mv -fv $TEMP/dotfiles/.bashrc		$HOME/.bashrc
+mv -fv $TEMP/dotfiles/.bash_profile	$HOME/.bash_profile
+mv -fv $TEMP/dotfiles/.vimrc		$HOME/.vimrc
+mv -fv $TEMP/dotfiles/.i3/config	$HOME/.i3/config
+mv     $TEMP/dotfiles/.config/*		$HOME/.config
 
 # Setting natural scrolling > Use xmodmap instead, look into libinput
 
@@ -38,24 +38,39 @@ then
     cd paru
     makepkg -si
     cd $TEMP
-else
-    echo 'Paru already installed, refreshing databases'
-    sudo pacman -Syy
+#else
+#    echo 'Paru already installed, refreshing databases'
+#    sudo pacman -Syy
 fi
 
 echo "Install packages at this time? (Y/n)"
 read yn
 while true; do
     case $yn in
-        [Yy]* ) ls dotfiles/packlist > packversion.txt
-            select PACKLIST in $(cat packversion.txt) exit; do
+        [Yy]* ) ls $TEMP/dotfiles/packlist > packversion.txt
+            select PACKLIST in $(cat packversion.txt) diff exit; do
                 case $PACKLIST in
+		    
+		diff) echo "Choose two packages to compare:" ; 
+			select PACK1 in $(cat packversion.txt); do echo "First package is $PACK1"; break; done;
+			    echo "Choose a second package"   	
+			select PACK2 in $(cat packversion.txt); do echo "Second package is $PACK2"; break; done; 
+				if ! command -v colordiff &> /dev/null;
+				then  diff	$TEMP/dotfiles/packlist/$PACK1 $TEMP/dotfiles/packlist/$PACK2 ; 	
+				else  colordiff $TEMP/dotfiles/packlist/$PACK1 $TEMP/dotfiles/packlist/$PACK2 ; 
+				fi
+			read -p "Press enter to continue"
+			echo "Install one of these packages?"
+			select PACKLIST in $PACK1 $PACK2; do echo "Installing $PACKLIST"; break; done;
+                        paru -S - < dotfiles/packlist/"$PACKLIST"
+			break ;;
+
                     exit) echo "Exiting."
                         break ;;
+
                     *) echo "$PACKLIST"
                         echo Installing "$PACKLIST";
                         paru -S - < dotfiles/packlist/"$PACKLIST"
-                        rm packversion.txt
                 esac
             done; break;;
         [Nn]* ) break;;
@@ -63,22 +78,21 @@ while true; do
     esac
 done
 
-if ! command -v nitrogen &> /dev/null
-then
-    echo 'Not setting wallaper'
-else
-    curl https://unsplash.com/photos/Dksk8szLRN0/ > $HOME/.default_wallpaper.png
-    nitrogen $HOME/.default_wallpaper.png
-    echo 'Wallpaper set.'
-fi
+#if ! command -v nitrogen &> /dev/null && ! command -v feh &> /dev/null ; then echo "No background backend"; 
+#else 		
+#	select BACKGROUND in Default URL; do 
+#		case $BACKGROUND in
+#		Default) URL=$('https://i.imgur.com/o6BcMBy.jpg'); break;;
+#		URL) echo "Please input image URL"; read URL ; break;
+#		esac; done	
+#	wget $URL > $TEMP/background.jpg;
+#	sudo mv $TEMP/background.jpg /usr/share/backgrounds/.background.jpg ;
+#	feh --bg-fill /usr/share/backgrounds/back.jpeg || nitrogen $HOME
+#fi
+	
 
 echo 'Cleaning up.'
 sleep 1
-rm -fdr $TEMP
-echo 'One moment please.'
-sleep 1
 echo "Setup complete, exiting deployrable."
 sleep 1
-
-rm -- "$0"
 exit 0
